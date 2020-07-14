05dc0fc12275f724a15a642a5a625d1638d92987
Stig Sandnes <stig_sandnes@hotmail.com>
2020-07-14 09:00:01 +0200

    Last layer

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 See https://tomdalling.com/toms-data-onion/
 
+Build requirements:
+sudo apt-get install libssl-dev
+
 ```
-mkdir build && cd build && cmake -D CMAKE_BUILD_TYPE=Debug .. && cmake --build . && cat ../start-page.txt | ./layer0 | ./layer1 | ./layer2 | ./layer3 | ./layer4
+mkdir build && cd build && cmake -D CMAKE_BUILD_TYPE=Debug .. && cmake --build . && cat ../start-page.txt | ./layer0 | ./layer1 | ./layer2 | ./layer3 | ./layer4 | ./layer5
 ```
---
+++ layer5.cpp
@@ -0,0 +1,43 @@
+#include "main_common.hpp"
+#include <openssl/evp.h>
+#include <array>
+
+int main()
+{
+    return main_common([] (auto & data) {
+        //enforce(data.size() >= 96);
+        std::array<unsigned char, 40> key;
+        {
+            auto ctx = EVP_CIPHER_CTX_new();  // EVP_CIPHER_CTX_free()
+            EVP_CIPHER_CTX_set_flags(ctx, EVP_CIPHER_CTX_FLAG_WRAP_ALLOW);  // must explicitly enable wrap mode!
+            int init_res = EVP_DecryptInit_ex(ctx, EVP_aes_256_wrap(), nullptr, reinterpret_cast<unsigned char *>(data.data()), reinterpret_cast<unsigned char *>(data.data() + 32));
+
+            int write_len;
+            int update_res = EVP_CipherUpdate(ctx, key.data(), &write_len, reinterpret_cast<unsigned char *>(data.data() + 40), 40);
+            assert(update_res);
+
+            int out_len = write_len;
+            int final_res = EVP_CipherFinal_ex(ctx, key.data() + out_len, &write_len);
+            assert(final_res == 1);
+            out_len += write_len;
+            assert(out_len == 32);
+        }
+
+        std::vector<std::byte> out(data.size());
+        {
+            auto ctx = EVP_CIPHER_CTX_new();  // EVP_CIPHER_CTX_free()
+            int init_res = EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), nullptr, key.data(), ((unsigned char *) data.data()) + 80);
+
+            int write_len;
+            int update_res = EVP_CipherUpdate(ctx, reinterpret_cast<unsigned char *>(out.data()), &write_len, reinterpret_cast<unsigned char *>(data.data() + 96), data.size() - 96);
+            assert(update_res);
+
+            int out_len = write_len;
+            int final_res = EVP_CipherFinal_ex(ctx, reinterpret_cast<unsigned char *>(out.data() + out_len), &write_len);
+            out_len += write_len;
+            out.resize(out_len);
+        }
+
+        return out;
+    });
+}

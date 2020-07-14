9fda9ac5af405ee78555a87a3ca754d195500b50
Stig Sandnes <stig_sandnes@hotmail.com>
2020-07-14 22:07:53 +0200

    fixxup layer 5

--- layer5.cpp
+++ layer5.cpp
@@ -6,14 +6,15 @@ int main()
 {
     return main_common([] (auto & data) {
         //enforce(data.size() >= 96);
+        auto in = reinterpret_cast<unsigned char const *>(data.data());
         std::array<unsigned char, 40> key;
         {
             auto ctx = EVP_CIPHER_CTX_new();  // EVP_CIPHER_CTX_free()
             EVP_CIPHER_CTX_set_flags(ctx, EVP_CIPHER_CTX_FLAG_WRAP_ALLOW);  // must explicitly enable wrap mode!
-            int init_res = EVP_DecryptInit_ex(ctx, EVP_aes_256_wrap(), nullptr, reinterpret_cast<unsigned char *>(data.data()), reinterpret_cast<unsigned char *>(data.data() + 32));
+            int init_res = EVP_DecryptInit_ex(ctx, EVP_aes_256_wrap(), nullptr, in, in + 32);
 
             int write_len;
-            int update_res = EVP_CipherUpdate(ctx, key.data(), &write_len, reinterpret_cast<unsigned char *>(data.data() + 40), 40);
+            int update_res = EVP_CipherUpdate(ctx, key.data(), &write_len, in + 40, 40);
             assert(update_res);
 
             int out_len = write_len;
@@ -26,10 +27,10 @@ int main()
         std::vector<std::byte> out(data.size());
         {
             auto ctx = EVP_CIPHER_CTX_new();  // EVP_CIPHER_CTX_free()
-            int init_res = EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), nullptr, key.data(), ((unsigned char *) data.data()) + 80);
+            int init_res = EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), nullptr, key.data(), in + 80);
 
             int write_len;
-            int update_res = EVP_CipherUpdate(ctx, reinterpret_cast<unsigned char *>(out.data()), &write_len, reinterpret_cast<unsigned char *>(data.data() + 96), data.size() - 96);
+            int update_res = EVP_CipherUpdate(ctx, reinterpret_cast<unsigned char *>(out.data()), &write_len, in + 96, data.size() - 96);
             assert(update_res);
 
             int out_len = write_len;
